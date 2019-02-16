b27da86095f5b145d2bce7cc98f7b9753d20b6a0
tigertv <maxvetrov555@yandex.ru>
2019-02-16 15:52:22 +0200

    Fix zigzag cipher
    It worked wrong with key=4

--- tests/test_zigzag.py
+++ tests/test_zigzag.py
@@ -5,41 +5,17 @@ from secretpy.zigzag import Zigzag
 import unittest
 
 class TestZigzag(unittest.TestCase):
-	alphabet = (u"abcdefghijklmnopqrstuvwxyz",
-		u"абвгдеёжзийклмнопрстуфхцчшщъыьэюя",
-		u"abcdefghijklmnopqrstuvwxyzäöüß",
-		u"abcdefghijklmnñopqrstuvwxyz",
-		(
-			u"あいうえお"
-			u"かきくけこ"
-			u"がぎぐげご"
-			u"さしすせそ"
-			u"ざじずぜぞ"
-			u"たちつてと"
-			u"だぢづでど"
-			u"なにぬねの"
-			u"はひふへほ"
-			u"ばびぶべぼ"
-			u"ぱぴぷぺぽ"
-			u"まみむめも"
-			u"やゆよ"
-			u"らりるれろ"
-			u"わを"
-			u"ん"
-			u"ゃゅょぁぇ"
-			u"じづ"
-		)
-	)
-
-	key = (3,3,3,3,3) 
+	key = (3,4,3,3,3,3) 
 
 	plaintext = (u"wearediscoveredfleeatonce",
+		u"defendtheeastwallofthecastle",
 		u"текстовоесообщение",
 		u"textnachtricht",
 		u"unmensajedetexto",
 		u"だやぎへぐゆぢ")
 
 	ciphertext = (u"wecrlteerdsoeefeaocaivden",
+		u"dttfsedhswotatfneaalhcleelee",
 		u"ттебиесоосощнеквое",
 		u"tnthetahrctxci",
 		u"uneenesjdtxomaet",
@@ -47,14 +23,14 @@ class TestZigzag(unittest.TestCase):
 
 	def test_encrypt(self):
 		algorithm = Zigzag() 
-		for i,alphabet in enumerate(self.alphabet):
-			enc = algorithm.encrypt(self.key[i], self.plaintext[i], alphabet)
+		for i,plaintext in enumerate(self.plaintext):
+			enc = algorithm.encrypt(self.key[i], plaintext)
 			self.assertEqual(enc, self.ciphertext[i])
 
 	def test_decrypt(self):
 		algorithm = Zigzag() 
-		for i,alphabet in enumerate(self.alphabet):
-			dec = algorithm.decrypt(self.key[i], self.ciphertext[i], alphabet)
+		for i,ciphertext in enumerate(self.ciphertext):
+			dec = algorithm.decrypt(self.key[i], ciphertext)
 			self.assertEqual(dec, self.plaintext[i])
 
 if __name__ == '__main__': 
