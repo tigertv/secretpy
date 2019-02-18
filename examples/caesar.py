789f2579c0af32fe0719d28e8193b54edb04ebdc
tigertv <maxvetrov555@yandex.ru>
2019-02-18 19:59:12 +0200

    Update Caesar example

--- examples/caesar.py
+++ examples/caesar.py
@@ -8,13 +8,29 @@ plaintext  = u"thequickbrownfoxjumpsoverthelazydog"
 key = 3
 cipher = Caesar()
 
+print(plaintext)
 enc = cipher.encrypt(plaintext, key, alphabet)
 print(enc)
 dec = cipher.decrypt(enc, key, alphabet)
 print(dec)
 
+print('=====================================')
+
+print(plaintext)
 # use default english alphabet
 enc = cipher.encrypt(plaintext, key)
 print(enc)
 dec = cipher.decrypt(enc, key)
 print(dec)
+
+'''
+Output:
+
+thequickbrownfoxjumpsoverthelazydog
+wkhtxlfneurzqirämxpsvryhuwkhodüögrj
+thequickbrownfoxjumpsoverthelazydog
+=====================================
+thequickbrownfoxjumpsoverthelazydog
+wkhtxlfneurzqiramxpsvryhuwkhodcbgrj
+thequickbrownfoxjumpsoverthelazydog
+'''
