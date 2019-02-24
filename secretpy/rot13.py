a6de4beada7dd9f5fa07456138654019fe6a5245
tigertv <maxvetrov555@yandex.ru>
2019-02-24 03:54:28 +0200

    Refactor Rot13
    Use bit shifting instead of division by 2
    Remove unwanted, use less operations

--- secretpy/rot13.py
+++ secretpy/rot13.py
@@ -8,12 +8,11 @@ class Rot13:
 
 	def __encDec(self, alphabet, text):
 		alphabet = alphabet or self.__alphabet
-		key = (int)(len(alphabet) / 2) 
+		key = len(alphabet) >> 1
 		if len(alphabet) % 2 == 1:
+			alphabet += alphabet[key]
 			key += 1
-			alphabet += alphabet[key-1]
-		ret = self.__caesar.encrypt(text, key, alphabet)
-		return ret
+		return self.__caesar.encrypt(text, key, alphabet)
 
 	def encrypt(self, text, key=None, alphabet=None):
 		return self.__encDec(alphabet, text)
