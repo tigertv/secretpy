ade46018c82752e00cfb8f83c461f60a9761073e
tigertv <maxvetrov555@yandex.ru>
2019-02-25 23:45:08 +0200

    Add LowerCase decorator for CryptMachine

--- secretpy/cmdecorators/__init__.py
+++ secretpy/cmdecorators/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
 from .uppercase import UpperCase
+from .lowercase import LowerCase
 from .nospaces import NoSpaces
 from .savespaces import SaveSpaces
 from .savecase import SaveCase
 
 __all__ = [
-	"UpperCase", "NoSpaces", "SaveSpaces", "SaveCase",
+	"UpperCase", "LowerCase", "NoSpaces", "SaveSpaces", "SaveCase",
 ]
---
+++ secretpy/cmdecorators/lowercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+from .decorator import AbstractMachineDecorator
+
+class LowerCase(AbstractMachineDecorator):
+
+	def encrypt(self, text):
+		return self._machine.encrypt(text.lower())
+
+	def decrypt(self, text):
+		return self._machine.decrypt(text.lower())
