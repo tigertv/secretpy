61f2c15f6d595b327019577d8842771f16e1e78c
tigertv <maxvetrov555@yandex.ru>
2019-02-26 00:07:34 +0200

    Add Hebrew alphabet with 5 final letters

--- examples/atbash.py
+++ examples/atbash.py
@@ -3,7 +3,7 @@
 
 from secretpy import Atbash
 from secretpy import CryptMachine
-
+from secretpy import alphabet
 import secretpy.cmdecorators as md
 
 def encdec(machine, plaintext):
@@ -20,15 +20,12 @@ cm = md.NoSpaces(md.UpperCase(cm))
 plaintext  = u"attackatdawn"
 encdec(cm, plaintext)
 
-alphabet = u"אבגדהוזחטיךכלםמןנסעףפץצקרשת"
 plaintext  = u"במקום"
-cm.setAlphabet(alphabet)
+cm.setAlphabet(alphabet.HEBREW)
 encdec(cm, plaintext)
 
 plaintext  = u"The Fox jumps in Zoo too Achtung minen"
-
-alphabet = u"abcdefghijklmnopqrstuvwxyzäöüß"
-cm.setAlphabet(alphabet)
+cm.setAlphabet(alphabet.GERMAN)
 encdec(cm, plaintext)
 
 plaintext  = u"Achtung Minen"
