22e5a3eb426ba249164cfdcbe0d12245ebe82205
tigertv <maxvetrov555@yandex.ru>
2019-03-12 09:57:14 +0200

    Change the license

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
 	},
 	classifiers=[
 		'Development Status :: 3 - Alpha',
-		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+		'License :: OSI Approved :: MIT License',
 		'Intended Audience :: Developers',
 		'Intended Audience :: Education',
 		'Intended Audience :: Science/Research',
