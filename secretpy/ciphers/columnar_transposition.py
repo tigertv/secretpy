3678ccf821bb5dd9ccd939329c23428f3230a8d0
tigertv <maxvetrov555@yandex.ru>
2019-05-17 12:42:16 +0200

    Refactor Columnar Transportation
    Removed redundant operations:
      - sorted and multiplication in decrypt function.

--- secretpy/ciphers/columnar_transposition.py
+++ secretpy/ciphers/columnar_transposition.py
@@ -9,18 +9,19 @@ class ColumnarTransposition:
     """
 
     def __dec(self, alphabet, key, text):
-        # add endings to the text to fill the square
         chars = [alphabets.get_index_in_alphabet(char, alphabet)
                  for char in key]
         keyorder = sorted(enumerate(chars), key=lambda x: x[1])
         ret = u""
         rows = int(len(text) / len(key))
-        cols = [(text[i * rows: (i + 1) * rows], keyorder[i][0])
-                for i in range(len(key))]
-        cols2 = sorted(cols, key=lambda x: x[1])
+
+        cols = [0] * len(key)
+        for i, item in enumerate(range(0, len(text), rows)):
+            cols[keyorder[i][0]] = text[item: item + rows]
+
         for j in range(rows):
-            for i in range(len(cols2)):
-                ret += cols2[i][0][j]
+            for i in range(len(cols)):
+                ret += cols[i][j]
         return ret
 
     def __enc(self, alphabet, key, text):
