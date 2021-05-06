8364253ecd3f33b1f0cd737e1171754d9228e8a7
tigertv <maxvetrov555@yandex.ru>
2021-05-06 20:10:04 +0200

    Update Columnar Transposition

--- secretpy/ciphers/columnar_transposition.py
+++ secretpy/ciphers/columnar_transposition.py
@@ -12,17 +12,11 @@ class ColumnarTransposition:
         chars = [alphabets.get_index_in_alphabet(char, alphabet)
                  for char in key]
         keyorder = sorted(enumerate(chars), key=lambda x: x[1])
-        ret = u""
         rows = int(len(text) / len(key))
-
-        cols = [0] * len(key)
-        for i, item in enumerate(range(0, len(text), rows)):
-            cols[keyorder[i][0]] = text[item: item + rows]
-
-        for j in range(rows):
-            for i in range(len(cols)):
-                ret += cols[i][j]
-        return ret
+        roworder = [0] * len(key)
+        for i, (j, _) in enumerate(keyorder):
+            roworder[j] = i * rows
+        return u"".join(text[i+j] for j in range(rows) for i in roworder)
 
     def __enc(self, alphabet, key, text):
         # add endings to the text to fill the square
@@ -32,13 +26,8 @@ class ColumnarTransposition:
 
         chars = [alphabets.get_index_in_alphabet(char, alphabet)
                  for char in key]
-        keyorder1 = sorted(enumerate(chars), key=lambda x: x[1])
-
-        ret = u""
-        for i in range(len(key)):
-            ret += text[keyorder1[i][0]::len(key)]
-
-        return ret
+        keyorder = sorted(enumerate(chars), key=lambda x: x[1])
+        return u"".join(text[i::len(key)] for i, _ in keyorder)
 
     def encrypt(self, text, key, alphabet=None):
         """
