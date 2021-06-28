9872d3015d465d28d8ec703c512e85c7a42b68ce
tigertv <maxvetrov555@yandex.ru>
2021-06-28 19:06:32 +0200

    Fix RemoveNonAlphabet decorator
    - Fixed for alphabets of type 'tuples' or 'list'

--- secretpy/cmdecorators/remove_non_alphabet.py
+++ secretpy/cmdecorators/remove_non_alphabet.py
@@ -7,13 +7,24 @@ from .decorator import AbstractMachineDecorator
 class RemoveNonAlphabet(AbstractMachineDecorator):
 
     def encrypt(self, text):
-        return self.__encDec(text, lambda text: self._machine.encrypt(text))
+        return self.__crypt(text, lambda text: self._machine.encrypt(text))
 
     def decrypt(self, text):
-        return self.__encDec(text, lambda text: self._machine.decrypt(text))
+        return self.__crypt(text, lambda text: self._machine.decrypt(text))
 
-    def __encDec(self, text, func):
+    def __crypt(self, text, func):
         alphabet = self._machine.get_alphabet()
-        text2 = filter(lambda char: char in alphabet, text.lower())
-        res = func(text2)
-        return res
+
+        # filter text by alphabet
+        if isinstance(alphabet, str):
+            text2 = filter(lambda char: char in alphabet, text.lower())
+        else:
+            # for tuples or lists
+            txt = []
+            for c in text.lower():
+                for a in alphabet:
+                    if c in a:
+                        txt.append(c)
+            text2 = "".join(txt)
+            
+        return func(text2)
