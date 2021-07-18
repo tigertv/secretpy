d65bf8a6ed1ba0d17ef1ceaa689503d93e4bee77
tigertv <maxvetrov555@yandex.ru>
2021-07-18 15:29:59 +0200

    Fix CryptMachine for mixedcase alphabet

--- secretpy/cryptmachine.py
+++ secretpy/cryptmachine.py
@@ -1,49 +1,26 @@
 #!/usr/bin/python
 
 from .abstractmachine import AbstractCryptMachine
-from secretpy import alphabets
+from secretpy import alphabets as al
 
 
 class CryptMachine(AbstractCryptMachine):
-    def __init__(self, cipher, key="", alphabet=alphabets.ENGLISH):
+    def __init__(self, cipher, key="", alphabet=al.ENGLISH):
         self.__key = key
         self.set_cipher(cipher)
         self.set_alphabet(alphabet)
 
-    def set_key(self, key):
-        self.__key = key
-
-    def set_alphabet(self, alphabet=alphabets.ENGLISH):
-        if hasattr(self.__cipher, 'get_fixed_alphabet'):
-            return
-        self.__alphabet = alphabet
-        self.mixedcase = False
-
-    def get_alphabet(self):
-        return self.__alphabet
-
-    def set_cipher(self, cipher):
-        self.__cipher = cipher
-        if not hasattr(self.__cipher, 'get_fixed_alphabet'):
-            return
-        alphabet = cipher.get_fixed_alphabet()
+    def __define_mixedcase(self, alphabet):
         # check upper case letters
         has_upper = False
         has_lower = False
 
-        if isinstance(alphabet, str):
-            for c in alphabet:
+        for letters in alphabet:
+            for c in letters:
                 if c.isupper():
                     has_upper = True
                 elif c.lower():
                     has_lower = True
-        else:
-            for letters in alphabet:
-                for c in letters:
-                    if c.isupper():
-                        has_upper = True
-                    elif c.lower():
-                        has_lower = True
 
         if has_upper and has_lower:
             self.__alphabet = alphabet
@@ -54,6 +31,24 @@ class CryptMachine(AbstractCryptMachine):
         else:
             self.__alphabet = alphabet
             self.mixedcase = False
+
+    def set_key(self, key):
+        self.__key = key
+
+    def set_alphabet(self, alphabet=al.ENGLISH):
+        if hasattr(self.__cipher, 'get_fixed_alphabet'):
+            return
+        self.__define_mixedcase(alphabet)
+
+    def get_alphabet(self):
+        return self.__alphabet
+
+    def set_cipher(self, cipher):
+        self.__cipher = cipher
+        if not hasattr(cipher, 'get_fixed_alphabet'):
+            return
+        alphabet = cipher.get_fixed_alphabet()
+        self.__define_mixedcase(alphabet)
 
     # returns True if mixedcase in alphabet, False otherwise
     def has_mixedcase(self):
