2129d6233ae2745eea9101d4fcab436f27f0cec7
tigertv <maxvetrov555@yandex.ru>
2021-07-18 12:16:00 +0200

    Update Bifid example

--- examples/bifid.py
+++ examples/bifid.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
-from secretpy import Bifid, CryptMachine, alphabets
+from secretpy import Bifid, CryptMachine, alphabets as al
+from secretpy.cmdecorators import UpperCase, SaveAll, Block
 
 
 def encdec(machine, plaintext):
@@ -23,8 +24,10 @@ alphabet = [
     u"1", u"2", u"3", u"4", u"5", u"6"
 ]
 cm.set_alphabet(alphabet)
-plaintext = u"текст"
+plaintext = u"Текст здесь, много!!!"
 encdec(cm, plaintext)
+
+cm1 = Block(cm)
 
 alphabet = [
     u"p", u"h", u"q", u"g", u"m",
@@ -33,10 +36,11 @@ alphabet = [
     u"r", u"c", u"v", u"s", u"z",
     u"w", u"b", u"u", u"t", u"ij"
 ]
-cm.set_alphabet(alphabet)
-plaintext = u"defendtheeastwallofthecastle"
-encdec(cm, plaintext)
+cm1.set_alphabet(alphabet)
+plaintext = u"Defend the East Wall of the Castle!"
+encdec(cm1, plaintext)
 
+cm1 = UpperCase(cm1)
 alphabet = [
     u"b", u"g", u"w", u"k", u"z",
     u"q", u"p", u"n", u"d", u"s",
@@ -44,31 +48,33 @@ alphabet = [
     u"f", u"c", u"l", u"u", u"m",
     u"t", u"h", u"y", u"v", u"r"
 ]
-cm.set_alphabet(alphabet)
-cm.set_key(10)
-plaintext = "fleeatonce"
-encdec(cm, plaintext)
+cm1.set_alphabet(alphabet)
+cm1.set_key(10)
+plaintext = "flee at once"
+encdec(cm1, plaintext)
 
-alphabet = alphabets.GREEK.upper()
-plaintext = u"ΠΙΝΑΚΑΣ"
+cm = SaveAll(cm)
+
+alphabet = al.GREEK_SQUARE
 cm.set_alphabet(alphabet)
+plaintext = u"Θέλει αρετή και τόλμη η ελευθερία. (Ανδρέας Κάλβος)"
 encdec(cm, plaintext)
 
 '''
-текст
-нит4я
-текст
+Текст здесь, много!!!
+нит4яжвыяьинтбф
+текстздесьмного
 ----------------------------------
+Defend the East Wall of the Castle!
+ffyhm khycp liash adtrl hcchl blr
 defendtheeastwallofthecastle
-ffyhmkhycpliashadtrlhcchlblr
-defendtheeastwallofthecastle
 ----------------------------------
-fleeatonce
-uaeolwrins
-fleeatonce
+flee at once
+UAEOL WRINS
+FLEEATONCE
 ----------------------------------
-ΠΙΝΑΚΑΣ
-ΡΛΖΠΣΕΓ
-ΠΙΝΑΚΑΣ
+Θέλει αρετή και τόλμη η ελευθερία. (Ανδρέας Κάλβος)
+Ζλζπρ οεπκφ ζιν μζυτυ η κλφδζγεγφ. (Πγδαργγ Ρυτακς)
+Θελει αρετη και τολμη η ελευθερια. (Ανδρεας Καλβος)
 ----------------------------------
 '''
