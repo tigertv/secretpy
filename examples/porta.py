597a343450f064df5614d788d5a4d9c0db5988fe
tigertv <maxvetrov555@yandex.ru>
2021-07-19 13:14:20 +0200

    Support non-string alphabets in Porta cipher

--- examples/porta.py
+++ examples/porta.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
+from secretpy import Porta, CryptMachine, alphabets as al
+from secretpy.cmdecorators import UpperCase, Block, SaveAll
 
-from secretpy import Porta
-from secretpy import alphabets
 
-alphabet = alphabets.GERMAN
-plaintext = u"thequickbrownfoxjumpsoverthelazydog"
-key = u"dogs"
+alphabet = al.GERMAN
+plaintext = u"schweißgequältvomödentextzürnttypografjakob"
+key = u"schlüssel"
 
 cipher = Porta()
 print(plaintext)
@@ -18,23 +18,46 @@ print(dec)
 
 #######################################################
 
-print("----------------------------------")
 
-plaintext = u"attackatdawn"
-key = u"lemon"
+def encdec(machine, plaintext):
+    print("--------------------------------------------------------------------")
+    print(plaintext)
+    enc = machine.encrypt(plaintext)
+    print(enc)
+    print(machine.decrypt(enc))
 
-print(plaintext)
-enc = cipher.encrypt(plaintext, key)
-print(enc)
-dec = cipher.decrypt(enc, key)
-print(dec)
+
+cm0 = CryptMachine(cipher, key)
+
+cm = cm0
+cm.set_alphabet(al.ENGLISH)
+cm.set_key("keys")
+plaintext = "I don't love non-alphabet characters. I will remove all of them: ^,&@$~(*;?&#. Great!"
+encdec(cm, plaintext)
+
+cm = Block(cm, length=4, sep="::")
+plaintext = "This text is divided by blocks of length 5!"
+encdec(cm, plaintext)
+
+cm = SaveAll(cm0)
+plaintext = "I love non-alphabet characters. These are : ^,&@$~(*;?&#. That's it!"
+encdec(cm, plaintext)
+
+cm.set_alphabet(al.ENGLISH_SQUARE_IJ)
+plaintext = "Jj becomes Ii because we use ENGLISH_SQUARE_IJ!"
+encdec(cm, plaintext)
+
+cm.set_alphabet(al.JAPANESE_HIRAGANA)
+cm.set_key(u"かぎ")
+plaintext = u"text いろはにほへと ちりぬるを わかよたれそ つねならむ うゐのおくやま けふこえて あさきゆめみし ゑひもせす !"
+encdec(cm, plaintext)
+
+cm = UpperCase(cm)
+alphabet = al.GREEK
+cm.set_alphabet(alphabet)
+cm.set_key(u"κλειδί")
+plaintext = u"Θέλει αρετή και τόλμη η ελευθερία. (Ανδρέας Κάλβος)"
+encdec(cm, plaintext)
 
 '''
-thequickbrownfoxjumpsoverthelazydog
-dßwheputrkrnßöroznpgcvdübmzüöwhatvy
-thequickbrownfoxjumpsoverthelazydog
-----------------------------------
-attackatdawn
-seauvppaxtel
-attackatdawn
 '''
--- secretpy/ciphers/porta.py
+++ secretpy/ciphers/porta.py
@@ -1,38 +1,15 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
-
-from secretpy import alphabets
+from itertools import cycle
+from secretpy import alphabets as al
 
 
 class Porta:
     """
     The Porta Cipher
     """
-    def __encDec(self, alphabet, key, text):
-        ans = ""
-        for i, char in enumerate(text):
-            try:
-                keychari = alphabet.index(key[i % len(key)]) >> 1
-            except ValueError:
-                wrchar = key[i % len(key)].encode('utf-8')
-                raise Exception("Can't find char '" + wrchar + "' of text in alphabet!")
-            try:
-                textindex = alphabet.index(char)
-            except ValueError:
-                wrchar = char.encode('utf-8')
-                raise Exception("Can't find char '" + wrchar + "' of text in alphabet!")
-            half = len(alphabet) >> 1
-            half_alphabet = None
-            if textindex < half:
-                half_alphabet = alphabet[half:]
-                alphIndex = (textindex + keychari) % half
-            else:
-                half_alphabet = alphabet[0:half]
-                alphIndex = (textindex - keychari) % half
-            ans += half_alphabet[alphIndex]
-        return ans
 
-    def encrypt(self, text, key, alphabet=alphabets.ENGLISH):
+    def encrypt(self, text, key, alphabet=al.ENGLISH):
         """
         Encryption method
 
@@ -41,14 +18,31 @@ class Porta:
         :param alphabet: Alphabet which will be used,
                          if there is no a value, English is used
         :type text: string
-        :type key: integer
+        :type key: string
         :type alphabet: string
         :return: text
         :rtype: string
         """
-        return self.__encDec(alphabet, key, text)
+        # prepare alphabet for substitution
+        indexes = {c: i for i, letters in enumerate(alphabet) for c in letters}
+        # prepare key
+        key_indexes = (indexes[c] >> 1 for c in key)
+        half = len(alphabet) >> 1
+        res = []
+        for t, k in zip(text, cycle(key_indexes)):
+            try:
+                texti = indexes[t]
+            except ValueError:
+                wrchar = t.encode('utf-8')
+                raise Exception("Can't find char '" + wrchar + "' of text in alphabet!")
+            if texti < half:
+                i = (texti + k) % half + half
+            else:
+                i = (texti - k) % half
+            res.append(alphabet[i][0])
+        return u"".join(res)
 
-    def decrypt(self, text, key, alphabet=alphabets.ENGLISH):
+    def decrypt(self, text, key, alphabet=al.ENGLISH):
         """
         Decryption method
 
@@ -57,10 +51,9 @@ class Porta:
         :param alphabet: Alphabet which will be used,
                          if there is no a value, English is used
         :type text: string
-        :type key: integer
+        :type key: string
         :type alphabet: string
         :return: text
         :rtype: string
         """
-
-        return self.__encDec(alphabet, key, text)
+        return self.encrypt(text, key, alphabet)
